8e77773ae0b56cd6f83310ca247ff77286d7b58f
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-18 15:10:39 +0000

    Laravel Tutorial for Beginners #2 - Routes & Views

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Network</title>
+</head>
+
+<body>
+    <h1>Welcome to the ninja network</h1>
+    <p>click the button below to view the list of ninja.</p>
+
+    <a href="/ninjas" class="btn">Find Ninja !</a>
+</body>
+
+</html>
