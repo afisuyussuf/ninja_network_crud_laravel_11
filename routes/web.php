1710a9375f2121ace7312df6c68f65973c949312
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-20 11:02:34 +0000

    Laravel Tutorial for Beginners #5 - Layouts & Slots

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,10 @@ Route::get('/ninjas', function () {
     return view('ninjas.index', ['greeting' => 'hello', 'ninjas' => $ninjas]);
 });
 
+Route::get('/ninjas/create', function () {
+    return view('ninjas.create');
+});
+
 Route::get('/ninjas/{id}', function ($id) {
-    // do something with the id
-    // e.g. fetch a record from the db
     return view('ninjas.show', ['id' => $id]);
 });
