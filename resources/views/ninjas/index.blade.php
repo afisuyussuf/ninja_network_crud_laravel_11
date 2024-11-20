935320d82dffd59d004bc2ebcbfae971b8949ee2
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-20 00:07:15 +0000

    Laravel Tutorial for Beginners #4 - Blade Directives

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -9,19 +9,18 @@
 
 <body>
     <h2>Currently Available Ninjas</h2>
-    <p>{{ $greeting }}</p>
+
+    @if ($greeting == 'hello')
+        <p>Hi form inside the if statement</p>
+    @endif
 
     <ul>
-        <li>
-            <a href="/ninjas/{{ $ninjas[0]['id'] }}">
-                {{ $ninjas[0]['name'] }}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{ $ninjas[1]['id'] }}">
-                {{ $ninjas[1]['name'] }}
-            </a>
-        </li>
+        @foreach ($ninjas as $ninja)
+            <li>
+                <p>{{ $ninja['name'] }}</p>
+                <a href="/ninjas/{{ $ninja['name'] }}">View Details</a>
+            </li>
+        @endforeach
     </ul>
 </body>
 
