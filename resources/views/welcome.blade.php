684e922062c3f587f5073bf645b8bc2185a1766e
afisuyussuf <afisuyussuf370@gmail.com>
2024-11-21 14:48:23 +0000

    Laravel Tutorial for Beginners #7 - Adding CSS & Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
 </head>
 
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to the ninja network</h1>
     <p>click the button below to view the list of ninja.</p>
 
-    <a href="/ninjas" class="btn">Find Ninja !</a>
+    <a href="/ninjas" class="btn mt-4 inline-block">Find Ninjas!</a>
 </body>
 
 </html>
